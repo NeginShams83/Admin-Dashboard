e8a13173dd627b91acf4e22e9a8b519db09a2b22
negin shams <neginshams83@gamil.com>
2026-07-01 20:18:22 +0330

    style : style sidbar dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,52 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 function Dashboard() {
   return (
     <div>
-      <h1>its dashboard</h1>
+      <div className="flex min-h-screen">
+        {/* sidebar */}
+        <aside className="w-60 border-r-2 border-sky-950 p-5">
+          <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+
+          <nav className="flex flex-col gap-4">
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-blue-700 text-white p-2 rounded-lg"
+                  : " hover:bg-blue-400  p-2 rounded-lg"
+              }
+              to={"profile"}
+            >
+              Profile
+            </NavLink>
+            <NavLink
+              to={"settings"}
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-blue-700 text-white p-2 rounded-lg"
+                  : " hover:bg-blue-400  p-2 rounded-lg"
+              }
+            >
+              Setting
+            </NavLink>
+            <NavLink
+              to={"favorites"}
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-blue-700 text-white p-2 rounded-lg"
+                  : " hover:bg-blue-400 p-2 rounded-lg"
+              }
+            >
+              Favorite
+            </NavLink>
+          </nav>
+        </aside>
+
+        {/* content */}
+        <main className="flex-1 p-8">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
