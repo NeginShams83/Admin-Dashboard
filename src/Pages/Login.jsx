698eae1d9fd18e2876975380adeda1776d54a86b
negin shams <neginshams83@gamil.com>
2026-07-04 00:54:30 +0330

    Style : add button

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import Input from "../Components/Input";
 function Login() {
   return (
     <>
-      <div className="flex flex-col items-center  justify-center h-96 bg-gray-300  rounded-2xl gap-5  text-black">
+      <div className="flex flex-col items-center  justify-center h-96 bg-[#B4B5BB]  rounded-2xl gap-5  text-black">
         {" "}
         <h1 className="text-2xl mb-10">Welcome to the Login</h1>
         <Input
@@ -12,7 +12,7 @@ function Login() {
           type="text"
           placeholder="Enter your username..."
           className={
-            "m-4 outline-none text-white px-4 py-1 bg-blue-900 rounded-lg "
+            "m-4 outline-none text-white focus:ring-1 ring-white px-4 py-1 bg-[#385894] transition-all hover:bg-[#2c3e70]  rounded-lg "
           }
         />
         <Input
@@ -21,9 +21,12 @@ function Login() {
           type="password"
           placeholder="Enter your password..."
           className={
-            "m-4 outline-none text-white px-4 py-1 bg-blue-900 rounded-lg "
+            "m-4 outline-none text-white focus:ring-1 ring-white px-4 py-1 bg-[#385894] hover:bg-[#2c3e70] transition-all rounded-lg "
           }
         />
+        <button className="bg-[#385894] text-white px-5 py-1 rounded-lg hover:bg-[#2c3e70] transition-all ">
+          Login
+        </button>
       </div>
     </>
   );
