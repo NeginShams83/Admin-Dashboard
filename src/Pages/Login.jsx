37661ae0715356fb3daa5dfaa45d6011b4ef5bbb
negin shams <neginshams83@gamil.com>
2026-07-03 23:34:33 +0330

    style: create login page and style input components

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,31 @@
+import Input from "../Components/Input";
+
 function Login() {
   return (
-    <div>
-      <h1>login page</h1>
-    </div>
+    <>
+      <div className="flex flex-col items-center  justify-center h-96 bg-gray-300  rounded-2xl gap-5  text-black">
+        {" "}
+        <h1 className="text-2xl mb-10">Welcome to the Login</h1>
+        <Input
+          id="username"
+          label="Username :"
+          type="text"
+          placeholder="Enter your username..."
+          className={
+            "m-4 outline-none text-white px-4 py-1 bg-blue-900 rounded-lg "
+          }
+        />
+        <Input
+          id="password"
+          label="Password :"
+          type="password"
+          placeholder="Enter your password..."
+          className={
+            "m-4 outline-none text-white px-4 py-1 bg-blue-900 rounded-lg "
+          }
+        />
+      </div>
+    </>
   );
 }
 
