a4406f0e53a83aa23a2aef503bbc05673f89fecc
negin shams <neginshams83@gamil.com>
2026-07-16 20:58:47 +0330

    feat(ui):add modal

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,11 +1,16 @@
 import { useTheme } from "../context/ThemeContext";
+import { useState } from "react";
+import Modal from "../Components/Modal.jsx";
+import EditForm from "../Components/EditForm.jsx";
+import Button from "../Components/Button.jsx";
 
 function Settings() {
   //get theme from context
   const { theme, toggleTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen ">
       <h2>تنظیمات تم: {theme}</h2>
       <button
         className={`${theme === "dark" ? "bg-blue-950 text-white" : "bg-blue-400 text-white"} px-4 py-1 rounded-lg`}
@@ -13,6 +18,12 @@ function Settings() {
       >
         change theme
       </button>
+      <Button onClick={() => setIsOpen(true)}>Edit Profile</Button>
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <EditForm onCancel={() => setIsOpen(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
