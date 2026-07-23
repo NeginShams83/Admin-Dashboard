c5f0a73132e225ba0ccb7ddcd378ec47c1d352df
negin shams <neginshams83@gamil.com>
2026-07-23 20:38:25 +0330

    feat: add EditForm component with form validation and api integration

--- src/Components/EditForm.jsx
+++ src/Components/EditForm.jsx
@@ -1,31 +1,126 @@
 import Input from "./Input";
 import Button from "./Button";
+import { useEffect, useState } from "react";
+import api from "../api/api.js";
 
-function EditForm({ onCancel, onUpdate }) {
+function EditForm({ onCancel, onSave, profile, userId }) {
+  //formData
+  const [formData, setFormData] = useState({
+    firstname: "",
+    username: "",
+    password: "",
+  });
+  //error state
+  const [firstnameError, setFirstnameError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  //useEffect
+  useEffect(() => {
+    if (!profile) return;
+    setFormData({
+      firstname: profile.firstname,
+      username: profile.username,
+      password: "",
+    });
+  }, [profile]);
+
+  //handle sub
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setFirstnameError("");
+    setUsernameError("");
+    setPasswordError("");
+
+    let hasError = false;
+    //
+    if (!formData.firstname.trim()) {
+      setFirstnameError("firstname required");
+      hasError = true;
+    }
+    if (!formData.username.trim()) {
+      setUsernameError("username required");
+      hasError = true;
+    }
+    if (!formData.password.trim()) {
+      setPasswordError("Password required");
+      hasError = true;
+    }
+    if (hasError) return;
+    console.log({
+      formData,
+    });
+
+    try {
+      const res = await api.patch(`/users/${userId}`, formData);
+      console.log(res.data);
+      onSave?.();
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+  const handleCancel = () => {
+    setFormData({
+      firstname: profile.firstname || "",
+      username: profile.username,
+      password: "",
+    });
+    setUsernameError("");
+    setFirstnameError("");
+    setPasswordError("");
+
+    onCancel();
+  };
+
   return (
     <div className="w-full">
       <h2 className="text-2xl font-bold text-center  mb-6">Edit Profile</h2>
 
-      <form onSubmit={onUpdate} className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <Input
-          label="Username"
+          value={formData.firstname}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              firstname: e.target.value,
+            })
+          }
+          error={firstnameError}
+          label="firstname"
           className="w-full text-black bg-gray-50 border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
         />
 
         <Input
-          label="Old Password"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              username: e.target.value,
+            })
+          }
+          error={usernameError}
+          label="username"
           className="w-full text-black bg-gray-50 border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
         />
 
         <Input
-          label="New Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              password: e.target.value,
+            })
+          }
+          error={passwordError}
+          label="Password"
           className="w-full text-black bg-gray-50 border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
         />
 
         <div className="flex justify-end gap-3 mt-2">
           <Button
             type="button"
-            onClick={onCancel}
+            onClick={handleCancel}
             className="px-5 py-2 rounded-lg border border-gray-300 bg-white text-gray-700 hover:bg-gray-100 transition"
           >
             Cancel
--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,13 +1,48 @@
 import { useTheme } from "../context/ThemeContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "../Components/Modal.jsx";
 import EditForm from "../Components/EditForm.jsx";
 import Button from "../Components/Button.jsx";
+import { useAuth } from "../Context/AuthContext.jsx";
+import api from "../api/api.js";
 
 function Settings() {
   //get theme from context
   const { theme, toggleTheme } = useTheme();
   const [isOpen, setIsOpen] = useState(false);
+
+  const { user } = useAuth();
+  const [profile, setProfile] = useState(null);
+
+  //get
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await api.get(`/users/${user._id}`);
+
+        console.log(res.data);
+        setProfile(res.data.data.user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (user?._id) {
+      getUser();
+    }
+  }, [user]);
+
+//onSave
+  const handleSave = (updatedUser) => {
+    setProfile(updatedUser);
+    setIsOpen(false);
+  };
+
+
+  //test
+  useEffect(() => {
+    console.log("Profile:", profile);
+  }, [profile]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
@@ -21,7 +56,12 @@ function Settings() {
       <Button onClick={() => setIsOpen(true)}>Edit Profile</Button>
       {isOpen && (
         <Modal onClose={() => setIsOpen(false)}>
-          <EditForm onCancel={() => setIsOpen(false)} />
+          <EditForm
+            profile={profile}
+            userId={user._id}
+            onSave={handleSave}
+            onCancel={() => setIsOpen(false)}
+          />
         </Modal>
       )}
     </div>
