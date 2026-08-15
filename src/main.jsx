793e8c266a44da02d32a37ef10b032ac96af7ae1
negin shams <neginshams83@gamil.com>
2026-08-15 14:26:04 +0330

    feat(context): add ThemeProvider to root layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthProvider from "./Context/AuthContext";
+import AuthProvider from "./Context/AuthContext.jsx";
+import { ThemeProvider } from "./Context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </StrictMode>,
 );
